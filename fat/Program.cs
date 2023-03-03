e018830b7b391bea27caaa8c6b60059c7e0e8b47
Ana Cecilia Grossi Bolito <anac.grossi@icloud.com>
2023-03-03 13:54:42 -0300

    Add project files.

---
+++ fat/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, i=1;
+        Console.Write("Escolha um número para calcular o fatorial: ");
+        n = int.Parse(Console.ReadLine());
+        int aux = n;
+        while (i < n)
+        {
+           aux = aux * i;
+           i++;
+        }
+
+        Console.WriteLine(aux);
+    }
+}
---
+++ fatorial/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args, int fatN)
+    {
+        int n, i;
+        Console.Write("Escolha um número para calcular a fatorial: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (i = 1; i < n; i++)
+        {
+            n = n * i;
+
+        }
+
+        Console.WriteLine(n);
+}
+}
